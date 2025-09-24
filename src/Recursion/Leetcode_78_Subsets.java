9993b3ae834e564a70f3821874769bffbc165ab7
Vedansh-Agarwal-2007 <vedanshagarwaldpr1234@gmail.com>
2025-09-24 23:07:12 +0530

    updated code for the leecode 78

--- src/Recursion/Leetcode_78_Subsets.java
+++ src/Recursion/Leetcode_78_Subsets.java
@@ -1,23 +1,31 @@
 class Solution {
-    static List<List<Integer>> arr;
+    // Main function to generate all subsets of the given array
     public List<List<Integer>> subsets(int[] nums) {
-        arr = new ArrayList<>();
-        ArrayList<Integer> ans = new ArrayList<>();
-        allsubset(0,nums,ans);
-        return arr;
+        List<List<Integer>> res = new ArrayList<>(); // This list will store all subsets
+        List<Integer> subset = new ArrayList<>();   // Temporary list to store the current subset
+        allsubset(nums, 0, res, subset);            // Start recursion from index 0
+        return res;                                 // Return the final list of subsets
     }
-    public void allsubset(int i, int[] nums, ArrayList<Integer> ans){
-        if(i==nums.length){
-            ArrayList<Integer> list = new ArrayList<>();
-            for(int x=0;x<ans.size();x++){
-                list.add(ans.get(x));
-            }
-            arr.add(list);
-            return;
+
+    // Recursive helper function to generate subsets
+    private void allsubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
+        // Base case: if we've considered all elements in nums
+        if (index == nums.length) {
+            // Add a copy of the current subset to the result list
+            res.add(new ArrayList<>(subset));
+            return; // Return to the previous recursion level
         }
-        allsubset(i+1,nums,ans);
-        ans.add(nums[i]);
-        allsubset(i+1,nums,ans);
-        ans.remove(ans.size()-1);
+
+        // 1. Recursive call without including the current element (exclude it)
+        allsubset(nums, index + 1, res, subset);
+
+        // 2. Include the current element in the subset
+        subset.add(nums[index]);
+
+        // Recursive call after including the current element
+        allsubset(nums, index + 1, res, subset);
+
+        // 3. Backtrack: remove the last added element to try other possibilities
+        subset.remove(subset.size() - 1);
     }
 }
